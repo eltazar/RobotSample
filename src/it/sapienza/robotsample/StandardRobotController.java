f786ae0e3bec72c8003b69b473e9e4266cbe2141
Mario Greco <mrgreco3@gmail.com>
2012-05-25 21:08:14 +0200

    StandardRobotController: inserita seekbar per provare temporaneamente il robot

--- src/it/sapienza/robotsample/StandardRobotController.java
+++ src/it/sapienza/robotsample/StandardRobotController.java
@@ -6,10 +6,12 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
 
-public class StandardRobotController extends BaseActivity implements OnClickListener {
+public class StandardRobotController extends BaseActivity implements OnClickListener, OnSeekBarChangeListener {
 
 	private ProtocolAdapter pAdapt;
 	private Button aheadBtn;
@@ -17,7 +19,9 @@ public class StandardRobotController extends BaseActivity implements OnClickList
 	private Button leftBtn;
 	private Button rightBtn;
     private TextView messageFromServer;
-
+    private SeekBar bar;
+    private TextView textProgress;
+    
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,24 @@ public class StandardRobotController extends BaseActivity implements OnClickList
         
         messageFromServer = (TextView) findViewById(R.id.message_server);
         pAdapt = ProtocolAdapter.getInstance();
+        
+        bar = (SeekBar)findViewById(R.id.seekBar1); // make seekbar object
+        bar.setOnSeekBarChangeListener(this); // set seekbar listener.
+     // make text label for progress value
+        textProgress = (TextView)findViewById(R.id.textViewProgress);
+           
+    }
+    
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress,
+    		boolean fromUser) {
+    	// TODO Auto-generated method stub
+
+    	// change progress text label with current seekbar value
+    	textProgress.setText("The value is: "+(progress-128));
+    	sendMessage("#SPD0"+(progress-128)+"\r");
+    	sendMessage("#TRN00\r");
+    	// change action text label to changing
     }
 	
     @Override
@@ -43,13 +65,13 @@ public class StandardRobotController extends BaseActivity implements OnClickList
     	System.out.println("HO CLICCATO UN PULSANTE");
         switch ( v.getId() ) {
             case R.id.ahead:
-            	sendMessage("ahead");
+            	sendMessage("#armu\r");
             break;
             case R.id.back:
-            	sendMessage("back");
+            	sendMessage("#armd\r");
             break;
             case R.id.left:
-            	sendMessage("left");
+            	sendMessage("#SPD00\r");
             break;
             case R.id.right:
             	sendMessage("right");
@@ -72,5 +94,17 @@ public class StandardRobotController extends BaseActivity implements OnClickList
     	//aggiorno textView
     	messageFromServer.setText(answer);
     }
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+		
+	}
     
 }
