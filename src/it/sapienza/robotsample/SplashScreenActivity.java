dc60ea5e7314a2090751d65d12781e7fc53b8e4b
Mario Greco <mrgreco3@gmail.com>
2012-06-14 08:54:41 +0200

    SplashScreenActivity: iniziato inserimento dialog per mostrare scelta di connessione

--- src/it/sapienza/robotsample/SplashScreenActivity.java
+++ src/it/sapienza/robotsample/SplashScreenActivity.java
@@ -6,6 +6,8 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -13,6 +15,7 @@ import android.os.Message;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 import netInterface.NetworkUtility;
 import it.sapienza.robotsample.ProtocolAdapter;
 import netInterface.MessageIOStream;
@@ -22,7 +25,7 @@ public class SplashScreenActivity extends Activity implements OnClickListener{
 	private ProgressBar bar;
 	private ArrayList<String> scannedIp;
 	//private int mProgressStatus = 0;
-	
+
 	//gestore per i messaggi relativi alla progress bar
 	Handler handler = new Handler() {
 		@Override
@@ -34,45 +37,63 @@ public class SplashScreenActivity extends Activity implements OnClickListener{
 	};
 	//private Handler mHandler = new Handler();
 	private NetworkUtility netScan;
- 
+
 	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 		setContentView(R.layout.splashscreen);
-        
+
 		bar = (ProgressBar) findViewById(R.id.progress);
 		//associo al networkScanner l'handler
 		netScan = new NetworkUtility(handler,this);
+		
+		final CharSequence[] items = {"Esegui connessione manuale", "Riprova autoconnessione"};
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Non è stato possibile auto-connettersi al robot");
+		builder.setItems(items, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int item) {
+				//Toast.makeText(getApplicationContext(), items[item], Toast.LENGTH_SHORT).show();
+				if(item == 0){
+					launchConfig("manual");
+				}
+				else{
+					launchConfig("auto");
+				}
+			}
+		});
+		AlertDialog alert = builder.create();
+		alert.show();
 	}
- 
+
 	public void onStart() {
 		super.onStart();
 		bar.setProgress(0);
-		
+
 		//faccio partire la ricerca su un nuovo thread
-		new Thread(new Runnable(){
+		/*new Thread(new Runnable(){
 			public void run(){
 				scannedIp = netScan.doScan();
 				autoConnect();				
 			};
-		}).start();
-		
-//		 new Thread(new Runnable() {
-//             public void run() {
-//                 for(int i = 1;i< 255;i++){
-//                     mProgressStatus = netScan.scannerFasullo(i);
-//
-//                     // Update the progress bar
-//                     mHandler.post(new Runnable() {
-//                         public void run() {
-//                             bar.setProgress(mProgressStatus);
-//                         }
-//                     });
-//                 }
-//             }
-//         }).start();
+		}).start();*/
+
+		//		 new Thread(new Runnable() {
+		//             public void run() {
+		//                 for(int i = 1;i< 255;i++){
+		//                     mProgressStatus = netScan.scannerFasullo(i);
+		//
+		//                     // Update the progress bar
+		//                     mHandler.post(new Runnable() {
+		//                         public void run() {
+		//                             bar.setProgress(mProgressStatus);
+		//                         }
+		//                     });
+		//                 }
+		//             }
+		//         }).start();
 	}
- 
+
 	/*
 	 * Prova a connettersi in automatico con gli indirizzi ritornati dagli scanner alla ricerca 
 	 * di quello appartenente al robot
@@ -82,7 +103,7 @@ public class SplashScreenActivity extends Activity implements OnClickListener{
 		ProtocolAdapter pAdapt = ProtocolAdapter.getInstance();
 		String ack = "";
 		boolean isAutoconnected = false;
-				
+
 		for( String ip : scannedIp){
 			System.out.println("Indirizzo ip prova di connessione = "+ip);
 			MessageIOStream socket;
@@ -106,11 +127,11 @@ public class SplashScreenActivity extends Activity implements OnClickListener{
 					break;
 				}
 				//altrimenti chiudo socket e risorse associate
-	             socket.getMis().closeInput();
-	             socket.getMos().closeOutput();
-	             socket.close();
-	             pAdapt.setProtocolAdapter(null);				
-				
+				socket.getMis().closeInput();
+				socket.getMos().closeOutput();
+				socket.close();
+				pAdapt.setProtocolAdapter(null);				
+
 			} catch (UnknownHostException e) {
 				System.out.println(" Eccezione = "+e.getLocalizedMessage());
 				e.printStackTrace();
@@ -120,23 +141,47 @@ public class SplashScreenActivity extends Activity implements OnClickListener{
 				e.printStackTrace();
 			}
 		}
-		
-		 if(isAutoconnected == true){
-        	 //se autoconnessione avvenuta mostrare interfaccia con joystick
-        	 startActivity(new Intent(this, InterfacciaRobotActivity.class));
-         }
-         else{
-        	 //mostrare interfaccia configurazione
-        	 startActivity(new Intent(this, ConfigurationActivity.class));
-         }
+
+		if(isAutoconnected == true){
+			//se autoconnessione avvenuta mostrare interfaccia con joystick
+			// startActivity(new Intent(this, InterfacciaRobotActivity.class));
+
+//			final CharSequence[] items = {"Esegui connessione manuale", "Riprova autoconnessione"};
+//
+//			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//			builder.setTitle("Non è stato possibile auto-connettersi al robot");
+//			builder.setItems(items, new DialogInterface.OnClickListener() {
+//				public void onClick(DialogInterface dialog, int item) {
+//					//Toast.makeText(getApplicationContext(), items[item], Toast.LENGTH_SHORT).show();
+//					if(item == 0){
+//						launchConfig("manual");
+//					}
+//					else{
+//						launchConfig("auto");
+//					}
+//				}
+//			});
+//			AlertDialog alert = builder.create();
+//			alert.show();
+		}
+		else{
+			//mostrare interfaccia configurazione
+			startActivity(new Intent(this, ConfigurationActivity.class));
+		}
+	}
+
+	private void launchConfig(String mode){
+		if(mode.equals("manual")){
+			startActivity(new Intent(this, ConfigurationActivity.class));
+		}
 	}
 
 	@Override
-    public void onClick(View v) {
-        // TODO Auto-generated method stub
-        switch ( v.getId() ) {
-        	default:
-            break;
-        }
-    }
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch ( v.getId() ) {
+		default:
+			break;
+		}
+	}
 }
