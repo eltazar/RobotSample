cd62fff12570ebdd87f2d3b545fde18de13b0e18
eltazar <eltazar@eltazar.(none)>
2012-06-21 17:42:15 +0200

    aggiornato R.java

--- gen/it/sapienza/robotsample/R.java
+++ gen/it/sapienza/robotsample/R.java
@@ -9,59 +9,75 @@ package it.sapienza.robotsample;
 
 public final class R {
     public static final class attr {
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int customFont=0x7f010000;
     }
     public static final class drawable {
-        public static final int configbackground3=0x7f020000;
-        public static final int configbackground4=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int metalplate2=0x7f020003;
-        public static final int onscreen_control_base=0x7f020004;
-        public static final int onscreen_control_knob=0x7f020005;
-        public static final int tagmetal=0x7f020006;
+        public static final int add_icon=0x7f020000;
+        public static final int configbackground3=0x7f020001;
+        public static final int configbackground4=0x7f020002;
+        public static final int delete_icon=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int metalbackground=0x7f020005;
+        public static final int metalplate2=0x7f020006;
+        public static final int onscreen_control_base=0x7f020007;
+        public static final int onscreen_control_knob=0x7f020008;
+        public static final int rec_icon=0x7f020009;
+        public static final int segnali=0x7f02000a;
+        public static final int spinner=0x7f02000b;
+        public static final int spinner2=0x7f02000c;
+        public static final int stoptmp1=0x7f02000d;
+        public static final int stoptmp2=0x7f02000e;
+        public static final int tagmetal=0x7f02000f;
     }
     public static final class id {
         public static final int JoyPad=0x7f06001f;
-        public static final int ahead=0x7f060016;
-        public static final int autoconnectBtn=0x7f060007;
-        public static final int back=0x7f060017;
-        public static final int baseWV=0x7f060009;
-        public static final int config=0x7f06001d;
-        public static final int connect_button=0x7f06000f;
-        public static final int connectionBtn=0x7f060014;
-        public static final int connection_status=0x7f060011;
-        public static final int disconnect_button=0x7f060010;
-        public static final int disconnectionBtn=0x7f060008;
-        public static final int drag_layer=0x7f06000a;
-        public static final int edit_ipAddress=0x7f06000d;
-        public static final int edit_port=0x7f06000e;
-        public static final int imageView1=0x7f06000b;
-        public static final int imageView2=0x7f06000c;
-        public static final int ipAddress=0x7f060012;
-        public static final int ipRobot=0x7f060003;
-        public static final int left=0x7f060019;
-        public static final int manualConnection=0x7f060006;
-        public static final int message_server=0x7f06001a;
-        public static final int port=0x7f060013;
-        public static final int progress=0x7f060015;
-        public static final int progressbarConnection=0x7f060004;
-        public static final int right=0x7f060018;
-        public static final int seekBar1=0x7f06001b;
-        public static final int standardcontroller=0x7f06001e;
-        public static final int status=0x7f060001;
+        public static final int ahead=0x7f060018;
+        public static final int autoconnectBtn=0x7f06000b;
+        public static final int back=0x7f060007;
+        public static final int baseWV=0x7f06000d;
+        public static final int config=0x7f06001e;
+        public static final int connectionBtn=0x7f060016;
+        public static final int connection_status=0x7f060012;
+        public static final int disconnectionBtn=0x7f06000c;
+        public static final int drag_layer=0x7f06000e;
+        public static final int edit_ipAddress=0x7f060013;
+        public static final int edit_port=0x7f060015;
+        public static final int hack=0x7f060009;
+        public static final int imageView1=0x7f06000f;
+        public static final int imageView2=0x7f060010;
+        public static final int ipAddress=0x7f060011;
+        public static final int ipRobot=0x7f060005;
+        public static final int left=0x7f06001a;
+        public static final int manualConnection=0x7f06000a;
+        public static final int message_server=0x7f06001b;
+        public static final int port=0x7f060014;
+        public static final int portRobot=0x7f060004;
+        public static final int progress=0x7f060017;
+        public static final int progressbarConnection=0x7f060001;
+        public static final int rescan=0x7f060008;
+        public static final int right=0x7f060019;
+        public static final int searchTxt=0x7f060006;
+        public static final int seekBar1=0x7f06001c;
+        public static final int status=0x7f060003;
         public static final int test_image=0x7f060000;
         public static final int textView1=0x7f060002;
-        public static final int textView2=0x7f060005;
-        public static final int textViewProgress=0x7f06001c;
+        public static final int textViewProgress=0x7f06001d;
     }
     public static final class layout {
         public static final int automaticconnection=0x7f030000;
         public static final int choosetypeconnection=0x7f030001;
         public static final int disconnectconnection=0x7f030002;
         public static final int interfacciarobotactivity=0x7f030003;
-        public static final int main=0x7f030004;
-        public static final int manualconnection=0x7f030005;
-        public static final int splashscreen=0x7f030006;
-        public static final int standardrobotcontroller=0x7f030007;
+        public static final int manualconnection=0x7f030004;
+        public static final int splashscreen=0x7f030005;
+        public static final int standardrobotcontroller=0x7f030006;
     }
     public static final class menu {
         public static final int option_menu=0x7f050000;
@@ -74,4 +90,33 @@ public final class R {
         public static final int ipAddress=0x7f040000;
         public static final int port=0x7f040002;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a CustomTextView.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #CustomTextView_customFont it.sapienza.robotsample:customFont}</code></td><td></td></tr>
+           </table>
+           @see #CustomTextView_customFont
+         */
+        public static final int[] CustomTextView = {
+            0x7f010000
+        };
+        /**
+          <p>This symbol is the offset where the {@link it.sapienza.robotsample.R.attr#customFont}
+          attribute's value can be found in the {@link #CustomTextView} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:customFont
+        */
+        public static final int CustomTextView_customFont = 0;
+    };
 }
