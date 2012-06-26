0a5f5a1329023ad707655a9d712ee4c6407bc359
Mario Greco <mrgreco3@gmail.com>
2012-06-26 13:03:07 +0200

    InterfacciaRobotActivity: aggiunti metodi per mostrare mosaico webview

--- src/it/sapienza/robotsample/InterfacciaRobotActivity.java
+++ src/it/sapienza/robotsample/InterfacciaRobotActivity.java
@@ -1,5 +1,6 @@
 package it.sapienza.robotsample;
 
+import netInterface.NetworkUtility;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,14 +11,17 @@ import android.view.View;
 import android.view.View.OnTouchListener;
 import android.view.Window;
 import android.webkit.WebView;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class InterfacciaRobotActivity extends BaseActivity implements OnTouchListener {
 
 	private DragController mDragController;
 	private DragLayer mDragLayer;
 	private WebView baseWV;
+	private static InterfacciaRobotActivity IntRobot;
 
 	private float mCenter_x;
 	private float mCenter_y;
@@ -26,6 +30,7 @@ public class InterfacciaRobotActivity extends BaseActivity implements OnTouchLis
 	private final int activity_index = 0;
 	
 	private ProgressBar speedometer;
+	private ImageView webcam;
 	
 	//gestore per i messaggi relativi alla progress bar
 	private Handler handler = new Handler() {
@@ -38,12 +43,12 @@ public class InterfacciaRobotActivity extends BaseActivity implements OnTouchLis
 			
 
 			if(msg.what > oldValue){
-				System.out.println("ACCEL: ora √® = "+speedometer.getProgress()+"arrivato = "+msg.what+" incremento di = "+Math.abs(msg.what-speedometer.getProgress()));
+				System.out.println("ACCEL: ora è = "+speedometer.getProgress()+"arrivato = "+msg.what+" incremento di = "+Math.abs(msg.what-speedometer.getProgress()));
 				speedometer.incrementProgressBy(Math.abs(msg.what-speedometer.getProgress()));
 				oldValue = msg.what;
 			}
 			else if(msg.what < oldValue){
-				System.out.println("DECEL: ora √® = "+speedometer.getProgress()+"arrivato = "+msg.what+" decremento di = "+Math.abs(msg.what-speedometer.getProgress()));
+				System.out.println("DECEL: ora è = "+speedometer.getProgress()+"arrivato = "+msg.what+" decremento di = "+Math.abs(msg.what-speedometer.getProgress()));
 				speedometer.incrementProgressBy(-(Math.abs(msg.what-speedometer.getProgress())));
 				oldValue = msg.what;
 			}
@@ -72,6 +77,7 @@ public class InterfacciaRobotActivity extends BaseActivity implements OnTouchLis
 		
 		mDragController = new DragController(this, handler);
 		setUpViews();
+		IntRobot = this;
 	}
 	
 	@Override
@@ -114,9 +120,37 @@ public class InterfacciaRobotActivity extends BaseActivity implements OnTouchLis
 		mDragLayer = (DragLayer) findViewById(R.id.drag_layer);
 		mDragLayer.setDragController(dragController);
 		dragController.addDropTarget(mDragLayer);
+		webcam = (ImageView) findViewById(R.id.imageView3);
 
 		ImageView IW = (ImageView) findViewById(R.id.imageView2);
 
+		//controllo sulla webcam
+		webcam.setOnClickListener(new ImageView.OnClickListener() {  
+        public void onClick(View v)
+            {
+        	
+        		if(NetworkUtility.getInstance().getIpAddresses().size() == 0)
+        			Toast.makeText(getApplicationContext(), "Webcam non disponibile, effettuare una nuova scansione", Toast.LENGTH_LONG).show();
+        		else {
+        		FrameLayout frameLayout = (FrameLayout)findViewById(R.id.preview);
+        		findViewById(R.id.imageView2).setVisibility(WebView.GONE);
+        		
+                //lancia la nuova activity
+        		new WebcamHandler(frameLayout, IntRobot);
+        		}
+            }
+         });
+
+		//controllo sul joypad
 		IW.setOnTouchListener(this);
 	}
+	
+	//setta l'ip della webcam al valore selezionato dall'utente
+	public void setSelectedIp(String ipWebcam)
+	{
+		String finalIp = "http://rackbot:rackbot@" + ipWebcam + "/mobile.htm";
+		baseWV.loadUrl(finalIp);
+		findViewById(R.id.imageView2).setVisibility(WebView.VISIBLE);
+	}
 }
+
