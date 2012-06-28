1f0eb68328befc185bf1acc54c6fc235d4853046
Mario Greco <mrgreco3@gmail.com>
2012-06-28 16:24:25 +0200

    SplashscreenActivity: bug fix -> chiusa connessione se nn rispetta if

--- src/it/sapienza/robotsample/SplashScreenActivity.java
+++ src/it/sapienza/robotsample/SplashScreenActivity.java
@@ -208,15 +208,15 @@ public class SplashScreenActivity extends Activity implements OnClickListener{
 				//System.out.println("Substring ack = "+ack.substring(1, ack.length()));
 				
 				//controllo che il primo carattere sia 6 (in ascii = "ack")
-				if(ack != null && ack.length() != 0){
-					byte b = (byte)ack.charAt(0);
-
-					if(b==6){
-						//if(ack.substring(0, 10).equals("6RoborRack")){
-						System.out.println("AUTOCONNESSIONE RIUSCITA");
-						isAutoconnected = true;
-						break;
-					}	
+				if(ack != null && ack.length() != 0 && (byte)ack.charAt(0) == 6){
+					//byte b = (byte)ack.charAt(0);
+					//System.out.println("BYTE = "+b);
+					//if(b==6){
+					//if(ack.substring(0, 10).equals("6RoborRack")){
+					System.out.println("AUTOCONNESSIONE RIUSCITA");
+					isAutoconnected = true;
+					break;
+					//}	
 				}
 				else{
 					//altrimenti chiudo socket e risorse associate
