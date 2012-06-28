c4b3a756443a077febfeddb33ad1a5901b545294
Mario Greco <mrgreco3@gmail.com>
2012-06-28 17:16:07 +0200

    BaseActivity: ora istanza un nuovo intente solo se è null il precedente

--- src/it/sapienza/robotsample/BaseActivity.java
+++ src/it/sapienza/robotsample/BaseActivity.java
@@ -4,42 +4,52 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.webkit.WebView;
 
 /*
- * Si occupa di associare a ogni activty che la eredita lo stesso optionsMenu
- * */
+* Si occupa di associare a ogni activty che la eredita lo stesso optionsMenu
+* */
 public class BaseActivity extends Activity{
+	
+	private Intent config = null;
+	private Intent joypad = null;
+	private Intent voicecontrol = null;
 
 	
 	public boolean onCreateOptionsMenu(Menu menu) {
 
-        MenuInflater inflater = getMenuInflater();
+       MenuInflater inflater = getMenuInflater();
 
-        inflater.inflate(R.menu.option_menu, menu);
+       inflater.inflate(R.menu.option_menu, menu);
 
-        return true;
+       return true;
 
-    }
+   }
 	
 	public boolean onOptionsItemSelected(MenuItem item) {
-   	 
-        //respond to menu item selection
-    	switch (item.getItemId()) {
-    		case R.id.config:
-    			startActivity(new Intent(this, ConfigurationActivity.class));
-    			return true;
-    		case R.id.JoyPad:
-    			startActivity(new Intent(this, InterfacciaRobotActivity.class));
-    			return true;
-    		case R.id.voiceControl:
-    			startActivity(new Intent(this, VoiceControlActivity.class));
-    			return true;
-    		default:
-    			return super.onOptionsItemSelected(item);
-    	}
+  	
+       //respond to menu item selection
+   	switch (item.getItemId()) {
+   		case R.id.config:
+   			if(config == null)
+   				config = new Intent(this, ConfigurationActivity.class);
+   			startActivity(config);
+   			return true;
+   		case R.id.JoyPad:
+   			if(joypad == null)
+   				joypad = new Intent(this, InterfacciaRobotActivity.class);
+   			startActivity(joypad);
+   			return true;
+   		case R.id.voiceControl:
+   			if(voicecontrol == null)
+   				voicecontrol = new Intent(this, VoiceControlActivity.class);
+   			startActivity(voicecontrol);   			
+   			return true;
+   		default:
+   			return super.onOptionsItemSelected(item);
+   	}
 
-    }
+   }
+
 	
 	//setta l'ip della webcam al valore selezionato dall'utente
 	public void setSelectedIp(String ipWebcam)
