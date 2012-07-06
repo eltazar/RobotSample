f08b85a092d2ee234013f02197b28f5fad7a8ffc
Mario Greco <mrgreco3@gmail.com>
2012-07-06 23:24:25 +0200

    BaseActivity: inserito metodo per controllare se activity corrente è ultima activity dello stack

--- src/it/sapienza/robotsample/BaseActivity.java
+++ src/it/sapienza/robotsample/BaseActivity.java
@@ -1,9 +1,17 @@
 package it.sapienza.robotsample;
+import java.io.IOException;
+import java.util.List;
+
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 /*
 * Si occupa di associare a ogni activty che la eredita lo stesso optionsMenu
@@ -60,5 +68,59 @@ public class BaseActivity extends Activity{
 	//setta l'ip della webcam al valore selezionato dall'utente
 	public void setSelectedIp(String ipWebcam)
 	{
+	}
+	
+	private  void exit(){
+		try {
+			ProtocolAdapter.getInstance().closeCommunication();
+		} catch (IOException e) {
+			System.out.println("Errore disconnessione: "+e.getLocalizedMessage());
+		}
+	    this.finish();
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+	    if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+	    		    	
+	    	ActivityManager mngr = (ActivityManager) getSystemService( ACTIVITY_SERVICE );
+
+	    	List<ActivityManager.RunningTaskInfo> taskList = mngr.getRunningTasks(10);
+
+	    	System.out.println(this.getClass().getName()+": LISTA DI TASK = "+taskList.get(0).numActivities);
+	    	
+	    	if((taskList.get(0).numActivities == 1) && 
+	    			taskList.get(0).topActivity.getClassName().equals(this.getClass().getName())) {
+	    		
+    			System.out.println(this.getClass().getName() +" ultima activity dello stack");
+
+    			AlertDialog.Builder alertbox = new AlertDialog.Builder(this);
+    			alertbox.setTitle("RobotSample");
+    			alertbox.setMessage("Vuoi davvero uscire?");
+
+    			alertbox.setPositiveButton("Si",
+    					new DialogInterface.OnClickListener() {
+    				public void onClick(DialogInterface arg0, int arg1) {
+    					exit();
+    				}
+    			});
+
+    			alertbox.setNeutralButton("No",
+    					new DialogInterface.OnClickListener() {
+    				public void onClick(DialogInterface arg0, int arg1) {
+    				}
+    			});
+
+    			alertbox.show();
+    			
+    			return true;
+	    	}
+	    	else {
+		        return super.onKeyDown(keyCode, event);
+		    }
+	    	
+	    } else {
+	        return super.onKeyDown(keyCode, event);
+	    }
 	}
 }
