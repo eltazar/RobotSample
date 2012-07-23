c5c2fd3f25e2e49ea9b414fecccaf56ec772700f
Mario Greco <mrgreco3@gmail.com>
2012-07-23 16:07:01 +0200

    BaseActivity: aggiunto credits

--- src/it/sapienza/robotsample/BaseActivity.java
+++ src/it/sapienza/robotsample/BaseActivity.java
@@ -5,12 +5,19 @@ import java.util.List;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /*
@@ -57,6 +64,9 @@ public class BaseActivity extends Activity{
    			if(accelero == null)
    				accelero = new Intent(this, AccelerometroActivity.class);
    			startActivity(accelero);   			
+   			return true;
+   		case R.id.credits:
+   			credits(this); 			
    			return true;
    		default:
    			return super.onOptionsItemSelected(item);
@@ -123,4 +133,91 @@ public class BaseActivity extends Activity{
 	        return super.onKeyDown(keyCode, event);
 	    }
 	}
+	
+	public void credits(final Context context){
+		//creazione dialog
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.maindialog);
+        dialog.setTitle("CREDITS");
+        dialog.setCancelable(true);
+        
+        //bottoni nomi ed email
+        Button supervision = (Button) dialog.findViewById(R.id.ButtonS);
+        supervision.setOnClickListener(new OnClickListener() {
+        //@Override
+            public void onClick(View v) {
+        		sendEmail(context, new String[]{"panizzi@di.uniroma1.it"}, "Invio Email", "Android Montessori: ", "Testo email");
+            }
+        });
+        
+        Button developer = (Button) dialog.findViewById(R.id.ButtonD);
+        developer.setOnClickListener(new OnClickListener() {
+        //@Override
+            public void onClick(View v) {
+        		sendEmail(context, new String[]{"flaviarot88@gmail.com"}, "Invio Email", "Android Montessori: ", "Testo email");
+            }
+        });
+        
+        Button web = (Button) dialog.findViewById(R.id.ButtonW);
+        web.setOnClickListener(new OnClickListener() {
+        @Override
+            public void onClick(View v) {
+        	Intent web = new Intent("android.intent.action.VIEW", Uri.parse("http://www.google.it"));  
+        	context.startActivity(web);
+            }
+        });
+        
+        Button asilo = (Button) dialog.findViewById(R.id.ButtonA);
+        asilo.setOnClickListener(new OnClickListener() {
+        @Override
+            public void onClick(View v) {
+        	Intent web2 = new Intent("android.intent.action.VIEW", Uri.parse("http://www.casamontessoriirafi.com"));  
+        	context.startActivity(web2);
+            }
+        });
+        
+        Button andengine = (Button) dialog.findViewById(R.id.ButtonST);
+        andengine.setOnClickListener(new OnClickListener() {
+        //@Override
+            public void onClick(View v) {
+        	Intent web3 = new Intent("android.intent.action.VIEW", Uri.parse("http://www.andengine.org"));  
+        	context.startActivity(web3);
+            }
+        });
+        
+      //banner sapienza
+        ImageView img = (ImageView) dialog.findViewById(R.id.ImageView01);
+        img.setImageResource(R.drawable.sapienza);
+
+    /*    //creazione bottone chiusura
+        Button button = (Button) dialog.findViewById(R.id.Button01);
+        button.setOnClickListener(new OnClickListener() {
+        @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+     */   
+        dialog.show();
+       
+       
+	}
+	
+	//METODO PER INVIO EMAIL
+	public static void sendEmail(Context context, String[] recipientList,
+            String title, String subject, String body) {
+        Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+        emailIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        emailIntent.setType("plain/text");
+
+        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, recipientList);
+
+        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+
+        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, body);
+
+        context.startActivity(Intent.createChooser(emailIntent, title));
+
+}
 }
